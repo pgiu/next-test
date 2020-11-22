64b6f6b5aa58c2845871b0b78b4a30c4945093f8
Pablo Giudice <pablo@siempreencasa.com.ar>
2020-11-22 18:40:36 -0300

    added dynamic routes

--- pages/index.js
+++ pages/index.js
@@ -1,40 +1,44 @@
 import Head from 'next/head'
-import Layout, { siteTitle } from '../components/layout'
+import Layout, {siteTitle} from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
-import { getSortedPostsData } from '../lib/posts'
+import {getSortedPostsData} from '../lib/posts'
+import Link from 'next/link'
+import Date from '../components/date'
 
 export default function Home({allPostsData}) {
-  return (
-    <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
-      <section className={utilStyles.headingMd}>
-        <p>Bienvenidos a mi página</p>
-        <p>
-          Estoy probando Vercel a ver como se porta!
-        </p>
-      </section>
-        <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-            <h2 className={utilStyles.headingLg}>Blog</h2>
-            <ul className={utilStyles.list}>
-                {allPostsData.map(({id, date, title}) => (
-                    <li className={utilStyles.listItem} key={id}>
-                        {title}
-                        <br/>
-                        {id}
-                        <br/>
-                        {date}
-                    </li>
-                ))}
-            </ul>
-        </section>
-    </Layout>
+    return (
+        <Layout home>
+            <Head>
+                <title>{siteTitle}</title>
+            </Head>
+            <section className={utilStyles.headingMd}>
+                <p>Bienvenidos a mi página</p>
+                <p>
+                    Estoy probando Vercel a ver como se porta!
+                </p>
+            </section>
+            <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+                <h2 className={utilStyles.headingLg}>Blog</h2>
+                <ul className={utilStyles.list}>
+                    {allPostsData.map(({id, date, title}) => (
+                        <li className={utilStyles.listItem} key={id}>
+                            <Link href={`/posts/${id}`}>
+                                <a>{title}</a>
+                            </Link>
+                            <br/>
+                            <small className={utilStyles.lightText}>
+                                <Date dateString={date}/>
+                            </small>
+                        </li>
+                    ))}
+                </ul>
+            </section>
+        </Layout>
 
-  )
+    )
 }
 
-export async function getStaticProps(){
+export async function getStaticProps() {
     const allPostsData = getSortedPostsData()
     return {
         props: {
