f405811c7c8d4ee1b3cf94cb0c67cd5ccfc674c3
Pablo Giudice <pablo@siempreencasa.com.ar>
2020-11-23 19:41:06 -0300

    added sample api endpoint

--- pages/api/movie.js
+++ pages/api/movie.js
@@ -1,5 +1,7 @@
+const movies=[{name:'Star Wars Ep. 5'}, {name:'The Big Lebowsky'}]
+
 export default function handler(req, res) {
     res.statusCode = 200
     res.setHeader('Content-Type', 'application/json')
-    res.end(JSON.stringify({ name: 'John Doe' }))
+    res.end(JSON.stringify(movies))
 }
